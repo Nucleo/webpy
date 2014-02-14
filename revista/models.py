ab20251087333eedf1ca7c1c4d766a5e77d7a9ca
jordanzet <jordanzet@gmail.com>
2014-02-14 17:53:40 -0500

    creacion de modelos en revista y creacion de carpetas

--- revista/models.py
+++ revista/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Revista(models.Model):
+	nombre = models.CharField(, max_length=50)
+	
+	class Meta:
+		verbose_name = _('Revista')
+		verbose_name_plural = _('Revistas')
+
+	def __unicode__(self):
+		return self.nombre
+	
+
+class Articulo(models.Model):
+    nombre = models.CharField(, max_length=50)
+    comentario = models.TextField(max_length=400)
+    repuesta = models.TextField(max_length=400)
+    class Meta:
+        verbose_name = _('Articulo')
+        verbose_name_plural = _('Articulos')
+
+    def __unicode__(self):
+        return self.nombre
+
+
+
